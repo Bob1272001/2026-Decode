9541df83e96f796a4020fc7da43b634d185a37db
Thomas Jin <83102001+Bob1272001@users.noreply.github.com>
2025-11-08 14:19:00 -0500

    finished the driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop_All.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop_All.java
@@ -5,10 +5,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Teleop_All extends LinearOpMode {
     //Drive Base
-    private DcMotor frontLeft = null;
-    private DcMotor frontRight = null;
-    private DcMotor backLeft = null;
-    private DcMotor backRight = null;
+    private DcMotor frontLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor backLeftMotor = null;
+    private DcMotor backRightMotor = null;
 
     //Intake
     private DcMotor intakeMotor = null;
@@ -20,15 +20,36 @@ public class Teleop_All extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         //Drive Base
-        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
-        frontRight = hardwareMap.get(DcMotor.class, "front_right");
-        backLeft = hardwareMap.get(DcMotor.class, "back_left");
-        backRight = hardwareMap.get(DcMotor.class, "back_right");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "front_left");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "front_right");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "back_left");
+        backRightMotor = hardwareMap.get(DcMotor.class, "back_right");
 
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE); // Example, adjust as needed
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE); // Example, adjust as needed
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+
+        double drive = -gamepad1.left_stick_y; // Negate y for forward to be positive
+        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+
+        double frontLeftPower = drive + strafe + turn;
+        double frontRightPower = drive - strafe - turn;
+        double backLeftPower = drive - strafe + turn;
+        double backRightPower = drive + strafe - turn;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
+        backRightMotor.setPower(backRightPower);
+
 
         //Intake
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
@@ -36,6 +57,7 @@ public class Teleop_All extends LinearOpMode {
         //Catapult
         catapultMotor1 = hardwareMap.get(DcMotor.class, "catapultMotor1");
         catapultMotor2 = hardwareMap.get(DcMotor.class, "catapultMotor2");
+
 
     }
 }
